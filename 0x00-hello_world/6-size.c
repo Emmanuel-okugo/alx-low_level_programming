ebde9fd4576487b6d968d8b3ee0e571ea515f38a
Emmanuel-okugo <okugoemma@gmail.com>
2024-01-10 08:47:28 -0800

    update on betty

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,10 +6,10 @@
  */
 int main()
 {
-	printf("size of char: %d byte(s)\n",sizeof(char));
-	  printf("size of int: %d byte(s)\n",sizeof(int));
-	    printf("size of long: %d byte(s)\n",sizeof(long));
-	      printf("size of long long int: %d byte(s)\n",sizeof(long long int));
-	        printf("size of float: %d byte(s)\n",sizeof(float));
+	printf("size of a char: %d byte(s)\n", sizeof(char));
+	  printf("size of an int: %d byte(s)\n", sizeof(int));
+	    printf("size of a long: %d byte(s)\n", sizeof(long));
+	      printf("size of a long long int: %d byte(s)\n", sizeof(long long int));
+	        printf("size of a float: %d byte(s)\n", sizeof(float));
 		return (0);
 }
