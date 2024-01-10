f2aa6a86c4511b557756f5551aa76214422f91cc
Emmanuel-okugo <okugoemma@gmail.com>
2024-01-10 00:14:24 -0800

    betty again

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
-/* this program prints an output using puts*/
+/**
+ * main - Entry point
+ * Description: A c programme that prints with puts function
+ * Return: Always 0 (Success)
+*/
 
-int main()
+int main(void )
 {
+
 	puts("\"Programming is like building a multilingual puzzle");
 
-	return 0;
+	return (0);
 }
