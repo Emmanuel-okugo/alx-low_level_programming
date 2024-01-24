6f01f942d2055adde41e7db1bbd381c655d3b154
Emmanuel-okugo <okugoemma@gmail.com>
2024-01-24 14:22:00 -0800

    update on number of bytes and betty

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,12 +3,12 @@
 /**
  * main - entry point
  * description: using write to print text to standard output
- * return: one (successful)
+ * return: always 1 (successful)
  */
 
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 54);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 
 	return (1);
 }
