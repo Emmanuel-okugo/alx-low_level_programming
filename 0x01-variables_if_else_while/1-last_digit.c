3bec39a283db940fc121cb6b7c64b3c813019333
Emmanuel-okugo <okugoemma@gmail.com>
2024-02-12 18:12:51 -0800

    picking last number in a string of numbers with % operator

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,12 +17,12 @@ int main(void)
 	n = rand() - RAND_MAX / 2;
 
 	str = n % 10;
-	printf("\n Last digit of %d is %d ", n, str);
-		if (str > 5)
+	printf("\nLast digit of %d is %d ", n, str);
+	if (str > 5)
 		printf("and is greatr than 5\n");
-		else if (str < 6 && str != 0)
+	else if (str < 6 && str != 0)
 		printf("and is less than 6 and not 0\n");
-		else
-                printf("and is 0\n");
+	else
+		printf("and is 0\n");
 	return (0);
 }
