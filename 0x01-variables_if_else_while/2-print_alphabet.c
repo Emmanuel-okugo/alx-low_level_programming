ceb10cebe29c3309a794b23f0308eb2b48cb41e4
Emmanuel-okugo <okugoemma@gmail.com>
2024-02-16 09:07:49 -0800

    used putchar to display the alphabets in upper case

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 /**
  * main - entry point
- * description: using putchar function to print upper case alphabet 
+ * description: using putchar function to print upper case alphabet
  * return: always 0(success)
  */
-char chr = 'A';
-
 int main(void)
 {
-	for (chr = 'A'; chr < 'Z'; chr++)
+	char chr ='a';
+
+	for (chr = 'a'; chr < 'a'; chr++)
 	{
 		putchar(chr);
 	}
