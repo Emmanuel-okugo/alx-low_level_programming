433e497d50b59f9a052bfb249ef9024400d8371d
Emmanuel-okugo <okugoemma@gmail.com>
2024-02-18 03:03:01 -0800

    printing from 0 to 9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * descripton: printing from 0 to 9
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int num=0;
+
+	for (num=0; num<10; num++)
+	{
+		printf("%d", num);
+	}
+	printf("\n");
+
+	return (0);
+}
+
+
