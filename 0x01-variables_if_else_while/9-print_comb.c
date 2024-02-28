21b63f0372d8130c7ec19e7b0bf86841034b857e
emmanuel-okugo <okugoemma@gmail.com>
2024-02-28 12:34:25 +0000

    printing possible combinations of single numbers

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,21 +1,21 @@
 #include <stdio.h>
 #include <unistd.h>
 /**
- * main - entry point 
- * description: a program that prints all possible combinations of single-digit numbers
+ * main - entry point
+ * description: a program that prints possible combinations of single-digit numbers
  * Return: always 0 (success)
  */
 int main (void)
 {
 	int i;
 
-    for (i = 1; i < 10; i++)
-    {
-        putchar(',');
-        putchar(' ');
-        putchar(i + '0');
-    }
-    putchar('\n');
+	for (i = 1; i < 10; i++)
+	{
+		putchar(i + '0');
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
 
-    return (0);
+	return (0);
 }
