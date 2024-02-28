77dca8d530bd18dd1eed3027816b7270b5cbd7b2
emmanuel-okugo <okugoemma@gmail.com>
2024-02-28 11:06:56 +0000

    printing base16 digits

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - entry point 
+ * description: printing base16 digits with putchar
+ * Return: always 0 (success)
+ */
+int main()
+{
+}
+return (0);
+
