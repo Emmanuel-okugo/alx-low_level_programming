aa7d53ec0d7ab59bf91bb55aca5466499feda01e
emmanuel-okugo <okugoemma@gmail.com>
2024-03-02 13:25:29 +0000

    updated betty coding style

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,25 +6,25 @@
  */
 int main(void)
 {
-    int i;
+	int i;
+	
+	int j;
+	
+	for (i = 0; i <= 8; i++)
+	{
+		for (j = i + 1; j <= 9; j++)
+		{
+			putchar((char) ('0' + i));
+			putchar((char) ('0' + j));
+			if(i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
 
-    int j;
-
-    for (i = 0; i <= 8; i++)
-    {
-        for (j = i + 1; j <= 9; j++)
-        {
-            putchar((char) ('0' + i));
-            putchar((char) ('0' + j));
-            if(i != 8 || j != 9)
-            {
-                putchar(',');
-                putchar(' ');
-            }
-        }
-    }
-    putchar('\n');
-
-    return (0);
+	return (0);
 }
 
