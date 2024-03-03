dc0b7ddf43fe40d3b06edd639b11c96d9a51b9a4
emmanuel-okugo <okugoemma@gmail.com>
2024-03-03 12:54:20 +0000

    combination 4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+```c
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Prints all possible different combinations of three digits
+ * Numbers are separated by ", " and printed in ascending order
+ * Only putchar function is allowed, with a maximum of six times
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int i, j, k;
+
+    /* Loop through all possible combinations */
+    for (i = 0; i <= 7; i++)
+    {
+        for (j = i + 1; j <= 8; j++)
+        {
+            for (k = j + 1; k <= 9; k++)
+            {
+                /* Print digits */
+                putchar('0' + i);
+                putchar('0' + j);
+                putchar('0' + k);
+
+                /* Print separator if not the last combination */
+                if (i != 7 || j != 8 || k != 9)
+                {
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    /* Print newline */
+    putchar('\n');
+
+    return (0);
+}
+```
