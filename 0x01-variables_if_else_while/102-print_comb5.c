1df2abcb2b525855d75c7ab687c50b2b905b8b8e
emmanuel-okugo <okugoemma@gmail.com>
2024-03-05 04:37:10 +0000

    combination 5

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,37 +1,33 @@
 #include <stdio.h>
-/**
- * main - -entry point
- * description: printing with putchar
- * Return: always 0 (success)
- */
 
+/**
+ * main - prints all  combinations of two two-digit numbers 
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-	int i;
-
-	int j;
+	int i, j;
 
 	for (i = 0; i < 100; i++)
 	{
-		for (j = i + 1; j < 100; j++)
+		for (j = 0; j < 100; j++)
 		{
-			putchar((i / 10) + '0'); /* first digit of i */
-			putchar((i % 10) + '0'); /* second digit of i */
-			putchar(' ');
-			putchar((j / 10) + '0'); /* first digit of j */
-			putchar((j % 10) + '0'); /* second digit of j */
-
-			/* condition for printing comma and space */
-			if (!(i == 98 && j == 99))
+			if (i < j)
 			{
-				putchar(',');
+				putchar((i / 10) + 48);
+				putchar((i % 10) + 48);
 				putchar(' ');
+				putchar((j / 10) + 48);
+				putchar((j % 10) + 48);
+				if (i != 98 || j != 99)
+				{
+					putchar(',');
+					putchar(' ');
+				}
 			}
+
 		}
 	}
-
 	putchar('\n');
-
 	return (0);
 }
-
