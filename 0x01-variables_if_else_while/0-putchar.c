d3f8830c4254806806aa29f010823ae2f402244d
emmanuel-okugo <okugoemma@gmail.com>
2024-03-08 02:43:31 +0000

    nested loops

---
+++ 0x01-variables_if_else_while/0-putchar.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * description: displaying putchar
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	printf("_putchar");
+	printf("\n");
+
+	return (0);
+}
