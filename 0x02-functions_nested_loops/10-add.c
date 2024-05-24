ebd469c53f39ae1da9058f54fe8ec3209a192e15
emmanuel_okugo <okugoemma@gmail.com>
2024-05-24 17:06:11 +0000

    a function that adds two integers and returns the result

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @c: accepts first argument
+ * @d: accepts second argumant
+ * description: adds two numbers
+ * Return: sum
+ */
+
+int add(int c, int d)
+{
+	return(c + d);
+}
+
---
+++ 0x02-functions_nested_loops/10-main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - check the code
+ *
+ * Return: always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	n = add(89, 9);
+	printf("%d\n", n);
+	return (0);
+}
