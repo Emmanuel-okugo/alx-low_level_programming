4707ee26a98aa5afabdac056eb5a39fe8e9fee4b
emmanuel-okugo <okugoemma@gmail.com>
2024-06-07 19:54:18 +0000

    a program that sums the even fibanocci numbers starting from 1 and 2 and less than 4000000

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description: sum all even fibonacci less than 4000000
+ * Return: always 0 (success)
+ */
+
+int main(void)
+{
+	unsigned long n = 2;
+
+	unsigned long z = 1;
+
+	unsigned long sum = 0;
+
+
+	sum = n;
+
+
+	while (n < 4000000)
+	{
+		n = n + z;
+		if (n % 2 == 0)
+		{
+			sum = sum + n;
+		}
+		z = n - z;
+	}
+
+	printf("%lu\n", sum);
+
+	return (0);
+}
