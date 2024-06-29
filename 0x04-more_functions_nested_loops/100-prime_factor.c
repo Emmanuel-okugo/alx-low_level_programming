dd768a0122776aa9053cf4990927b4c3be590b55
emmanuel-okugo <okugoemma@gmail.com>
2024-06-29 10:10:27 +0000

    a program that finds and prints the largest prime factor of the number 612852475143, followed by a new line.

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <math.h>
+
+/**
+ * main - entry point
+ * description: fins the largest prime factor of a number
+ * return: always 0 (success)
+ */
+
+int main(void)
+{
+	long number = 612852475143;
+	long maxPrime = -1;
+	long i;
+	
+	/* Divide number by 2 until it becomes odd */
+	while (number % 2 == 0)
+	{
+		maxPrime = 2;
+		number /= 2;
+	}
+
+	/* Check for odd factors from 3 to sqrt(number) */
+	for (i = 3; i <= sqrt(number); i += 2)
+	{
+		while (number % i == 0)
+		{
+			maxPrime = i;
+			number = number / i;
+		}
+	}
+
+    /* This condition is to check if number is a prime number greater than 2 */
+	if (number > 2)
+		maxPrime = number;
+	
+	printf("%ld\n", maxPrime);
+	
+	return 0;
+}
