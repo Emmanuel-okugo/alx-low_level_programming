0c6e8e789bafd92208b96f5b1bb50e6cfa1746a8
emmanuel-okugo <okugoemma@gmail.com>
2024-07-12 22:43:22 +0000

    bettty documentation style

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
 /**
- * 0-reset_to_98.c - function entry point
- * @*n: pointer to passed to the function by reference
+ * reset_to_98.c - function entry point
+ * @n: pointer to passed to the function by reference
  * decription: resets a variable using a pointer to it
  *
  */
