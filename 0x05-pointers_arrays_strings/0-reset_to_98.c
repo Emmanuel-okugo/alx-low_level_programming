2e838b6c1e1f31209f7e822171041c488095b55c
emmanuel-okugo <okugoemma@gmail.com>
2024-07-12 22:38:52 +0000

    readme file and a function that takes a pointer as argument and modifies the variable it points to

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,11 @@
+/**
+ * 0-reset_to_98.c - function entry point
+ * @*n: pointer to passed to the function by reference
+ * decription: resets a variable using a pointer to it
+ *
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifndef RESEET_TO_98
+#define RESET_TO_98
+
+void reset_to_98(int *n);
+
+#endif
