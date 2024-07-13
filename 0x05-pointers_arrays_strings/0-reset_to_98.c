e7da033c669cd5b2da036f71ab3a4c32ce103d2c
emmanuel-okugo <okugoemma@gmail.com>
2024-07-13 06:28:19 +0000

    betty doc update

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,8 +1,9 @@
 /**
  * reset_to_98.c - function entry point
  * @n: parameter
- * decription: this function takes a pointer to an integer as
+ *decription: this function takes a pointer to an integer as
  * its parameter and updates the value to 98
+ *
  */
 
 void reset_to_98(int *n)
