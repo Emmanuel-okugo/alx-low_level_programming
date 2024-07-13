3b2815a87e76dd5ab89fbe6317337d5d1a010b19
emmanuel-okugo <okugoemma@gmail.com>
2024-07-13 07:38:58 +0000

    a function that returns the length of a string

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,11 @@
+#include <string.h>
+/**
+ * 2-strlen - entry point 
+ * @s: type char parameter
+ * description: a function that returns the length of a string
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	return(strlen(s));
+}
