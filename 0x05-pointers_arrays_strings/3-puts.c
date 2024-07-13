9303f31a09e8df750b318110ac0475cc334a787e
emmanuel-okugo <okugoemma@gmail.com>
2024-07-13 07:58:16 +0000

    reduced description to be less than 80 characters

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -3,7 +3,8 @@
 /**
  * _puts - entry point
  * @str: character parameter
- * description: a function that prits a string, followed by a new line, to stdout
+ * description: a function that prits a string, followed by a new line, 
+ * to stdout
  *
  */
 void _puts(char *str)
