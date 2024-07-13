300b04b15e98939f80f98b43368cfaf8b8f39582
emmanuel-okugo <okugoemma@gmail.com>
2024-07-13 07:56:21 +0000

    a function that prints a string, followed by a new line to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * _puts - entry point
+ * @str: character parameter
+ * description: a function that prits a string, followed by a new line, to stdout
+ *
+ */
+void _puts(char *str)
+{
+	puts(str);
+}
