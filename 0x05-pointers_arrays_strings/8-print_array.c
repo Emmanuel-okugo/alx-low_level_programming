d61e575d15d974e9a942e1bfa3b84a46f18ed7b9
emmanuel-okugo <okugoemma@gmail.com>
2024-07-14 04:11:01 +0000

    a functiom that prints n elements of an array of integers, followed by a new line

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+ * print_array - entry point
+ * @a: pinter parameter
+ * @n: integer parameter
+ * description: a function that prints n elemets of an arrar of integers
+ * followed by a new line
+ *
+ */
+
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", *a);
+		a++;
+
+		if (i != n-1)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+}
