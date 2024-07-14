8bd4af4f71574dc4d68e3b12ccaabf646d12ff54
emmanuel-okugo <okugoemma@gmail.com>
2024-07-14 04:32:26 +0000

    betty doc and coding update

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -7,12 +7,12 @@
  * @src: parameter
  * description: a function that copies thet string pointr, including
  * the terminal null byte(\0), to another pointer
- *
+ * Return: pointer to dest
  */
 
 char *_strcpy(char *dest, char *src)
 {
 	strcpy(dest, src);
-	
+
 	return (dest);
 }
