889de917ae3c9897204c18e6985a80562ffd94c4
emmanuel-okugo <okugoemma@gmail.com>
2024-07-20 17:23:21 +0000

    function that copies n bytes of strings to another variable

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strncpy - entry point
+ * @dest: destination of copied string
+ * @src: string to copy from
+ * @n: number of bytes to copy
+ * description: a function that copies n
+ * bytes of a string to another string variable
+ * Return: pointer to copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	return (strncpy(dest,src,n));
+}
