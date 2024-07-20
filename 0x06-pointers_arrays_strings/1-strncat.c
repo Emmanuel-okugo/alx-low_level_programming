d18b831663dfa5f035f4a9c1ef2ba42f5196be98
emmanuel-okugo <okugoemma@gmail.com>
2024-07-20 16:39:29 +0000

    function that concatenates n bytes of a string to another string

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,16 @@
+#include <string.h>
+
+/**
+ * _strncat - entry point for this function
+ * @dest: pointer to string parameter
+ * @src: string to concatenate to dest
+ * @n: number of bytes to concatenate
+ * description: a function that concatenates n byte of a string to another string
+ * Return: pointer to the concatenated string
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	return (strncat(dest, src, n));
+}
+	
