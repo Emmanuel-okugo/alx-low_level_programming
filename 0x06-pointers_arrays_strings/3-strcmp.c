c8944a14f1c4272d641239c36216647cf272da61
emmanuel-okugo <okugoemma@gmail.com>
2024-07-20 18:19:55 +0000

    function that compares two strings

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,15 @@
+#include <string.h>
+
+/**
+ * _strcmp - entry point for this function
+ * @s1: first string
+ * @s2: second string
+ * description: function tghat compares two string
+ * Return: equal (0),fist argument < second argument (<0)
+ * first argumet > second argument (>0)
+ */
+
+int _strcmp(char *s1, char *s2)
+{
+	return (strcmp(s1, s2));
+}
