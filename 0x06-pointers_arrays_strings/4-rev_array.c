a4e38000ade257455c81d09f465a530fefd4aa98
emmanuel-okugo <okugoemma@gmail.com>
2024-07-27 00:27:52 +0000

    a function that reverses the content of an array of integers

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/**
+ * reverse_array - entry point
+ * @a: pointer to array
+ * @n: number of elements in the array
+ * description: a function that reverses the numbers stored in an array
+ *
+ */
+
+void reverse_array(int *a, int n)
+{
+	int * addressl;
+	int count;
+	int hold_value;
+	int i;
+	int *fake_a;
+
+	fake_a = a;
+	count = n / 2;
+
+
+	/* finding the address of the laast memeber of the array */
+	for (i = 0; i < 13; i++)
+	{
+		if (i == 12)
+		{
+			addressl = fake_a;
+		}
+		fake_a++;
+	}
+
+	for (i = 0; i < count; i++)
+	{
+		hold_value = *a;
+		*a = *addressl;
+		*addressl = hold_value;
+
+		addressl--;
+		a++;
+	}
+
+}
