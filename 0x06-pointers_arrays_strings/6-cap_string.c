8b1a486bd8cac53c4fee1854f737b64876c5e570
emmanuel-okugo <okugoemma@gmail.com>
2024-08-08 19:54:43 +0000

    changed the string in 6-main.c used to check the function

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,4 +1,3 @@
-#include <stdio.h>
 #include <string.h>
 /**
  * cap_string - entry point for this code
@@ -14,40 +13,34 @@ char *cap_string(char *c)
 	int i, j, k;
 
 	/* checking if the first character if the string is uppercase */
-	if (c[0] >= 97 && c[0] <= 122)
+	for (k = 0; k < 26; k++)
 	{
-		for (k = 0; k < 26; k++)
+		if (c[0] == lowercase)
 		{
-			if (c[0] == lowercase)
-			{
-				c[0] = uppercase;
-				break;
-			}
-			uppercase++;
-			lowercase++;
+			c[0] = uppercase;
+			break;
 		}
+		uppercase++;
+		lowercase++;
 	}
 
 	/* algorithm for converting others */
 	for (i = 1; i <= stringlength; i++)
 	{
-		if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n')
+		if (c[i] == ' ' || c[i] == '\t' || c[i] == '\n' || c[i] == '.')
 		{
-			if (c[i + 1] >= 97 && c[i + 1] <= 122)
+			uppercase = 'A';
+			lowercase = 'a';
+			
+			for (j = 0; j < 26; j++)
 			{
-				uppercase = 'A';
-				lowercase = 'a';
-
-				for (j = 0; j < 26; j++)
+				if (c[i + 1] == lowercase)
 				{
-					if (c[i + 1] == lowercase)
-					{
-						c[i + 1] = uppercase;
-						break;
-					}
-					uppercase++;
-					lowercase++;
+					c[i + 1] = uppercase;
+					break;
 				}
+				uppercase++;
+				lowercase++;
 			}
 		}
 	}
