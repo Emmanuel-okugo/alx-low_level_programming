2f5f72e1aa6a53fb1a09a64e6c6d5126aa28f33e
emmanuel-okugo <okugoemma@gmail.com>
2025-07-03 12:58:07 +0000

    101-print_number

--- 0x06-pointers_arrays_strings/101-print_number.c
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -2,6 +2,7 @@
 
 /**
  * print_number: entry point to this function
+ * @n: variable that contains the number
  * description: prints numbers using _putchar only
  * return: void
  */
@@ -11,6 +12,7 @@ void print_number(int n)
 	int size = 1;
 	int div;
 	int module;
+	int max_int_value = 1000000000;
 
 	/* printing - for negative values */
 	if (n < 0)
@@ -18,18 +20,18 @@ void print_number(int n)
 		_putchar('-');
 	}
 
-	/* finding the size of the number */
-	while (n / (size * 10) != 0) /* checking wether the future divisor is zero */
+	/* finding the size(place value) of the number */
+	while ((n / (size * 10) != 0) && ((size * 10) <= max_int_value)) /* checking wether the future divisor is zero */
 	{
 		size *= 10;
 	}
 
 	/* getting and printing the number */
-	while(size > 0)
+	while (size > 0)
 	{
 		div = n / size;
 		module = div % 10;
-		
+
 		if (module < 0)
 		{
 			_putchar('0' + (-module));
@@ -38,7 +40,7 @@ void print_number(int n)
 		{
 			_putchar('0' + module);
 		}
-		
+
 		size = size/10;
 	}
 }
